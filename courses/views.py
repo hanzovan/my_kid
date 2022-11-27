2a96ca89927f627d1e683a02a53f72ac58a9fa4d
Archer <thivan.nms@gmail.com>
2022-11-27 20:58:13 +0700

    Add static and styles

--- courses/views.py
+++ courses/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "courses/index.html")
+
+# Make it mobile responsive using media query
